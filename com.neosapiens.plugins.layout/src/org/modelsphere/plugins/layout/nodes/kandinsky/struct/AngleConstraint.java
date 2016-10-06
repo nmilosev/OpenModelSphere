6b6cbd3685c252ad95aa85ee1169eac925f6a570
Nemanja Milošević <nmilosev@prodesk.hp.nmilosev>
2016-10-06 11:56:59 +0200

    fix for linux systems

---
+++ com.neosapiens.plugins.layout/src/org/modelsphere/plugins/layout/nodes/kandinsky/struct/AngleConstraint.java
@@ -0,0 +1,17 @@
+package org.modelsphere.plugins.layout.nodes.kandinsky.struct;
+
+/*
+ * De�?nition 4.11 Let G = (V, E, F ) be a plane graph. An angle-constraint
+ * is a tuple (I, v, a, c). The �?rst entry I is a list of darts, which is an interval
+ * inside E(v) for v ∈ V . The entry a is an integer with 0 ≤ a ≤ 4 denoting the
+ * target-value for the sum of the angles de�?ned by the darts in the interval,
+ * and the last entry c is a non-negative integer, denoting a cost-factor for
+ * derivations of the target-value of the angle.
+ * Page 81
+ */
+/**
+ * The Class AngleConstraint.
+ */
+public class AngleConstraint {
+
+}
